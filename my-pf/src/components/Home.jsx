6dccba012d39f1822cfcee8014733d9bd4c341c8
Yasir Rabbani Tanvir <yasirrabbanitanvir@gmail.com>
2025-04-16 18:59:09 +0600

    update

--- my-pf/src/components/Home.jsx
+++ my-pf/src/components/Home.jsx
@@ -1,15 +1,55 @@
-import React from "react";
-import "../App.css"; 
+import React, { useEffect } from "react";
+import "../App.css";
+import MyPhoto from "../assets/me.jpg";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <section id="home" data-aos="fade-up">
-      <div className="jumbotron text-center">
-        <h1>Welcome to My Portfolio</h1>
-        <div className="name-animation">
-          <h2>Yasir Rabbani Tanvir</h2>
+      <div className="jumbotron">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-4 text-center mb-4 mb-md-0">
+              <img
+                src={MyPhoto}
+                alt="Yasir Rabbani Tanvir"
+                className="rounded-circle img-fluid shadow"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+            <div className="col-md-8 text-md-start text-center">
+              <h1 
+                style={{ fontSize: "2.5rem", fontWeight: "700", color: "#4B0082" }}
+                data-aos="fade-left"
+              >
+                Welcome to My Portfolio
+              </h1>
+              <div className="name-animation">
+                <h2
+                  style={{ fontSize: "2rem", fontWeight: "600", marginTop: "10px" }}
+                  data-aos="fade-right"
+                >
+                  Yasir Rabbani Tanvir
+                </h2>
+              </div>
+              <p 
+                style={{ fontSize: "1.1rem", color: "#555", marginTop: "10px" }}
+                data-aos="fade-left"
+              >
+                <strong>Frontend Developer</strong> | JavaScript | React.js
+              </p>
+            </div>
+          </div>
         </div>
-        <p>Frontend Developer | JavaScript | React.js </p>
       </div>
     </section>
   );
--- my-pf/src/components/Navbar.jsx
+++ my-pf/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Navbar, Nav } from 'react-bootstrap';
+import { FaBars } from 'react-icons/fa';
 import "../App.css"; 
 
 const CustomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <FaBars style={{ color: 'white', fontSize: '1.5rem' }} />
+      </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
